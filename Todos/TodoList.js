b5196811169d07154d89fcc5cc91efebb30b0694
Shenelle <sdelapaz09@outlook.com>
2023-08-05 14:00:55 -0700

    Todos

---
+++ Todos/TodoList.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Todo from "./Todo";
+import NewtodoForm from "./NewTodoForm";
+
+function TodoList(){
+    const [todos, setTodos] = useState([]);
+    const create = newTodo => {
+        setTodos(todos => [...todos, newTodo]);
+    };
+
+    const update = (id, updatedTask) => {
+        setTodos(todos => todos.map(todo => todo.id === id ? {...todo, task: updateTask } : todo ));
+    };
+
+    const remove = id => {
+        setTodos(todos => todos.filter(todo => todo.id !== id));
+    };
+
+    const todoComponents = todos.map(todo => (
+        <Todo
+            remove = {remove}
+            key = {todo.id}
+            id = {todo.id}
+            task = {todo.task}
+            update = {update}
+        />
+    ));
+
+    return (
+        <div>
+            <NewtodoForm createTodo = {create} />
+            <ul>{todoComponents}</ul>
+        </div>
+    );
+}
+
+export default TodoList;
